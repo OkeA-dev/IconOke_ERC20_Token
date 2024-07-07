238d1d28a8c716429a244322b554ed3616f42df8
Oke.A <Okea.dev@gmail.com>
2024-07-07 07:43:35 +0100

    IconToken tests

--- test/TestIconToken.t.sol
+++ test/TestIconToken.t.sol
@@ -6,6 +6,7 @@ import {IconToken} from "../src/IconToken.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/interfaces/draft-IERC6093.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract TestIconToken is Test {
     uint256 constant SEND_AMOUNT = 1 ether;
@@ -17,6 +18,8 @@ contract TestIconToken is Test {
     address Alice = makeAddr("alice");
     address Bob = makeAddr("bob");
     address zeroAddress = address(0);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     function setUp() external {
         s_deployIconToken = new DeployIconToken();
@@ -38,11 +41,10 @@ contract TestIconToken is Test {
     //////////////////////////////////////////////////////////////*/
 
     function testTransfer() public {
-        
         // Transfer tokens between accounts
         vm.prank(msg.sender);
         s_iconToken.transfer(Alice, SEND_AMOUNT);
-        
+
         // Transfer to the zero address (should fail)
         vm.expectRevert("Invalid Address");
         s_iconToken.transfer(zeroAddress, SEND_AMOUNT);
@@ -53,7 +55,6 @@ contract TestIconToken is Test {
 
         // Transfer zero token
         s_iconToken.transfer(Alice, 0);
-
 
         assertEq(s_iconToken.balanceOf(Alice), SEND_AMOUNT);
     }
@@ -80,10 +81,42 @@ contract TestIconToken is Test {
         vm.prank(Bob);
         vm.expectRevert();
         s_iconToken.transferFrom(msg.sender, Bob, ALLOWANCE_AMOUNT);
-        
 
         assertEq(s_iconToken.allowance(msg.sender, Bob), SEND_AMOUNT);
         assertEq(s_iconToken.balanceOf(Bob), SEND_AMOUNT);
     }
+
+    /*//////////////////////////////////////////////////////////////
+                           Event Emission
+    //////////////////////////////////////////////////////////////*/
+
+    function testEventEmission() public {
+        vm.expectEmit(true, true, false, true, address(s_iconToken));
+        emit Transfer(msg.sender, Alice, SEND_AMOUNT);
+        vm.prank(msg.sender);
+        s_iconToken.transfer(Alice, SEND_AMOUNT);
+    }
+
+    function testNoEventEmission() public {
+        vm.recordLogs();
+
+        s_iconToken.balanceOf(msg.sender);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 0, "No event should have been emitted");
+    }
+
+    function testTransferFromEmitsEvents() public {
+        uint256 amount = 1 * 10**18;
+        vm.prank(msg.sender);
+        s_iconToken.approve(Bob, amount);
+        
+
+        vm.startPrank(Bob);
+        vm.expectEmit(true, true, false, true);
+        emit Transfer(msg.sender, Bob, amount);
+        s_iconToken.transferFrom(msg.sender, Bob, amount);
+        vm.stopPrank();
+    }
+
 
 }
